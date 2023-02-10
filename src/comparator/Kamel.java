256e7ae504a30bb38a991185ad16008f73afd07a
Julian Schumacher <Jules.media@outlook.de>
2023-02-10 11:27:38 +0100

    New stuff

---
+++ src/comparator/Kamel.java
@@ -0,0 +1,12 @@
+package comparator;
+
+public final class Kamel {
+    public final String name;
+
+    public final int alter;
+
+    public Kamel(String name, int alter) {
+        this.name = name;
+        this.alter = alter;
+    }
+}
---
+++ src/comparator/MyComparator.java
@@ -0,0 +1,12 @@
+package comparator;
+
+
+import java.util.Comparator;
+
+public class MyComparator implements Comparator<Kamel> {
+
+    @Override
+    public int compare(Kamel o1, Kamel o2) {
+
+    }
+}
